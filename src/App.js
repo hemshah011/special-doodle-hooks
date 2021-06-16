1594692ae5d7cd8d92d091d585ccc95982d4eea9
hemshah011 <hemshah011@gmail.com>
2021-06-16 14:49:50 +0530

    Timer using useRef , state and effect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import React from 'react'
-import FocusInput from './components/FocusInput';
+import HookTimer from './components/HookTimer';
 
 function App() {
   return (
     <div className="App">
-      <FocusInput/>
+      <HookTimer/>
     </div>
   );
 }
